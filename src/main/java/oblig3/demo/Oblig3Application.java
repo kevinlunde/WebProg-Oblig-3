b240b192ad1d2c8c62a0fe4d8c81db648e523987
kevin_lunde <kevinlunde@live.no>
2021-04-04 12:45:53 +0200

    Initialisering og legge til html

---
+++ src/main/java/oblig3/demo/Oblig3Application.java
@@ -0,0 +1,13 @@
+package oblig3.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig3Application.class, args);
+    }
+
+}
---
+++ src/test/java/oblig3/demo/Oblig3ApplicationTests.java
@@ -0,0 +1,13 @@
+package oblig3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
